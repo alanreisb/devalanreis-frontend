be7256b877b6423bf431a47e8441b1093b8016d9
Alan Reis <alanreisb@gmail.com>
2019-12-16 20:32:40 -0300

    Atualização de Cidade

--- src/pages/Contatos.js
+++ src/pages/Contatos.js
@@ -4,9 +4,11 @@ import MenuLateral from '../components/Todas/MenuLateral'
 import FormMessage from '../components/Contatos/form-message'
 // Imagens
 import Localizacao from '../components/imagens/contatos/placeholder.png'
+import Spot from '../components/imagens/contatos/spot.png'
 import fbIcon from '../components/imagens/icones contato/facebook.png'
 import wpIcon from '../components/imagens/icones contato/whatsapp.png'
 import lkIcon from '../components/imagens/icones contato/linkedin.png'
+
 // Classes
 const contatosLocalizacaoClasse = 'fonte-secundaria pt-4 mt-0 text-center '
 const contatosLocalizacaoSecaoClasse = ' pl-0 text-left mt-3'
@@ -37,27 +39,38 @@ class Contatos extends Component {
             <ul className={contatosLocalIcones}>
               <li className={contatosLocalIcone}>
                 <a href='https://www.facebook.com/devalanreis' target='_blank'>
-                  <img src={fbIcon} className='icones-contato mr-2 mb-3' />
+                  <img src={fbIcon} className='icones-contato mr-2 mb-1 pt-2' />
                   https://www.facebook.com/devalanreis
                 </a>
               </li>
               <li className={contatosLocalIcone}>
                 <a href='https://www.linkedin.com/in/alanreisb' target='_blank'>
-                  <img src={lkIcon} className='icones-contato mr-2 mb-3' />
+                  <img src={lkIcon} className='icones-contato mr-2 mb-1 pt-2' />
                   https://www.linkedin.com/in/alanreisb
                 </a>
               </li>
               <li className={contatosLocalIcone}>
-                <a href='tel:(12)98250-1585'>
+                <a href='tel:(12)98139-2330'>
                   <img
                     src={wpIcon}
-                    className='icones-contato mr-2 mb-3'
-                    title='(12)98250-1585'
+                    className='icones-contato mr-2 mb-1 pt-2'
+                    title='(12)98139-2330'
                   />
-                  Cel:(12)98250-1585
+                  Cel:(12)98139-2330
                 </a>
               </li>
             </ul>
+            <div className='d-flex flex-row'>
+              <img src={Spot} className='mr-3 mb-3' alt='' />
+              <h2>Localização</h2>
+            </div>
+            <iframe
+              className='mapa'
+              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d64543.1153744142!2d-45.14741399380834!3d-22.729608362515197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ccc9201d468adf%3A0xc9ce9c4b4546c622!2sLorena%2C%20SP!5e0!3m2!1spt-BR!2sbr!4v1576537760145!5m2!1spt-BR!2sbr'
+              height='250px'
+              frameborder='0'
+              allowfullscreen
+            />
           </div>
         </div>
         <FormMessage />
--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -15,7 +15,7 @@ class Sobre extends Component {
         
         <Texto titulo='Sobre mim'>Sou um profissional apaixonado por desafios que nunca desiste de algo que começa. Trabalhava na área administrativa anteriormente. Porém, sempre quis me aproximar da área de tecnologia. Desenvolvia pequenas aplicações VBA para automação de processos nas empresas em que trabalhei até que finalmente resolvi mudar cursando Análise e Desenvolvimento de Sistemas.<br /><br />
           Busco aprender toda a técnica disponível em grande parte do meu tempo livre. Costumo ser perfeccionista com detalhes, ao mesmo tempo procuro ser dinâmico ao executar uma tarefa.<br /><br />
-          Atualmente estou à procura de um estágio onde possa empregar todos os conhecimentos e habilidades adquiridas.</Texto>
+          Atualmente estou à procura de um vaga como desenvolvedor front- end junior.</Texto>
           
          
         <Piramide></Piramide>
