aaf7cc46b44178e2deeed3935c26bc255070cd9d
Alan Reis - Desktop <alanreisb@gmail.com>
2020-04-17 21:22:09 -0300

    aa

--- src/api/enviar-email.js
+++ src/api/enviar-email.js
@@ -1,20 +1,22 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require('axios')
+require('dotenv').config()
 
-export function EnviarEmail(nome,email,mensagem) {
-    axios.post(`${process.env.REACT_APP_API_URL}`, {
-        nome: nome,
-        email: email,
-        mensagem: mensagem
+export function EnviarEmail (nome, email, mensagem) {
+  axios
+    .post(`${process.env.REACT_APP_API_URL}`, {
+      nome: nome,
+      email: email,
+      mensagem: mensagem
     })
-        .then(function (response) {
-            if(response.status === 200){
-                window.alert('Obrigado pelo contato. \n Mensagem foi enviada!');
-            }
-           
-        })
-        .catch(function (error) {
-            window.alert('Desculpe, ocorreu um erro. \n Por favor tente outra forma de contato.')
-               console.log(error);
-        });
-};
+    .then(function (response) {
+      if (response.status === 200) {
+        window.alert('Obrigado pelo contato. \n Mensagem foi enviada!')
+      }
+    })
+    .catch(function (error) {
+      window.alert(
+        'Desculpe, ocorreu um erro. \n Por favor tente outra forma de contato.'
+      )
+      console.log(error)
+    })
+}
