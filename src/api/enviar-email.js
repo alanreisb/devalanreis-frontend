9f54be326aa65f5e3d53b1dbceab785e04332259
Alan Reis - Desktop <alanreisb@gmail.com>
2020-04-17 23:06:59 -0300

    Voltar Node versão

--- src/api/enviar-email.js
+++ src/api/enviar-email.js
@@ -1,6 +1,7 @@
-const axios = require('axios')
-require('dotenv').config()
+const axios = require('axios');
+require('dotenv').config();
 
+<<<<<<< HEAD
 export function EnviarEmail (nome, email, mensagem) {
   console.log(`${process.env.REACT_APP_API_URL}`)
   axios
@@ -21,3 +22,22 @@ export function EnviarEmail (nome, email, mensagem) {
       console.log(error.message)
     })
 }
+=======
+export function EnviarEmail(nome,email,mensagem) {
+    axios.post(`${process.env.REACT_APP_API_URL}`, {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+    })
+        .then(function (response) {
+            if(response.status === 200){
+                window.alert('Obrigado pelo contato. \n Mensagem foi enviada!');
+            }
+           
+        })
+        .catch(function (error) {
+            window.alert('Desculpe, ocorreu um erro. \n Por favor tente outra forma de contato.')
+               console.log(error);
+        });
+};
+>>>>>>> parent of aaf7cc4... aa
