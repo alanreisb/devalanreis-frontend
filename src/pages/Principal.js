779750e39c0425ebe9d14f62be8fbf5f7753dceb
Alan Reis - Desktop <alanreisb@gmail.com>
2020-04-17 23:15:35 -0300

    Reverter Versão

--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -1,40 +1,31 @@
-import React, { Component } from 'react';
-import Media from 'react-media';
-/*Componentes*/
-import Menu from '../components/Principal/nav-principal';
-import MenuLateral from '../components/Todas/MenuLateral';
-import Titulo from '../components/Principal/titulo';
-import Player from '../components/Principal/player';
-
-
+import React, { Component } from 'react'
+import Media from 'react-media'
+/* Componentes */
+import Menu from '../components/Principal/nav-principal'
+import MenuLateral from '../components/Todas/MenuLateral'
+import Titulo from '../components/Principal/titulo'
+import Player from '../components/Principal/player'
 
 class Principal extends Component {
-    render() {
-        return (
-            <div className='pagina-principal'>
-                <Media query="(max-width: 599px)">
-                    {matches =>
-                        matches ? (
-                            <MenuLateral></MenuLateral>
+  render () {
+    return (
+      <div className='pagina-principal'>
+        <Media query='(max-width: 599px)'>
+          {matches => (matches ? <MenuLateral /> : <Menu />)}
+        </Media>
 
-                        ) : (
-                                <Menu></Menu>
-                            )
-                    }
-                </Media>
+        <Titulo />
+        <div className='cover'>
+          <img
+            src={require('../components/imagens/BlackHole.gif')}
+            className='imagem'
+          />
+        </div>
 
-
-                <Titulo></Titulo>
-                <div className='cover'>
-                    <img src={require('../components/imagens/BlackHole.gif')} className='imagem'></img>
-                </div>
-
-                <Player></Player>
-
-
-            </div>
-        );
-    }
+        <Player />
+      </div>
+    )
+  }
 }
 
-export default Principal;
+export default Principal
